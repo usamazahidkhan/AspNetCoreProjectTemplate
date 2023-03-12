6160d604369d1b088c851f47eb5dc24cc081c298
usamakhan9106 <usamakhan9106@hotmail.com>
2023-03-12 21:20:18 +0500

    PrimaryKey Updated

--- src/Domain/PrimaryKeyGenerator.cs
+++ src/Domain/PrimaryKeyGenerator.cs
@@ -1,26 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjectTemplate.Domain
+﻿namespace ProjectTemplate.Domain
 {
     internal class PrimaryKey
     {
         public static string GetUniqueNumStr()
         {
-            return Guid.NewGuid().GetHashCode().ToString().Trim('-');
+            return GetGuid().GetHashCode().ToString().Trim('-');
         }
 
         public static int GetUniqueNum()
         {
-            return Guid.NewGuid().GetHashCode();
+            return GetGuid().GetHashCode();
         }
 
         public static string GetGuidStr()
         {
-            return Guid.NewGuid().GetHashCode().ToString();
+            return GetGuid().GetHashCode().ToString();
         }
 
         public static Guid GetGuid()
