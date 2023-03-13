40640c308d7bfdc3aff15addda5d57565602ae6a
usamakhan9106 <usamakhan9106@hotmail.com>
2023-03-13 23:15:49 +0500

    Menu updated

--- src/Dashboard/PageRoutes.cs
+++ src/Dashboard/PageRoutes.cs
@@ -3,12 +3,11 @@
     public class PageRoutes
     {
         public const string dashboard = "/Dashboard";
-        public const string gettingStarted = "/GettingStarted";
-        public const string users = "/AllUsers";
+        public const string roles = "/Roles";
+        public const string users = "/Users";
         public const string createUser = users + "/Create";
         public const string updateUser = users + "/Update";
         public const string payments = "/Payments";
-        public const string support = "/Support";
         public const string profile = "/Profile";
         public const string logout = "/Logout";
     }
